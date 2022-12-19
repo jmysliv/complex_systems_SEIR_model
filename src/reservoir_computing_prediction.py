f6c75f90a16cab60cc3e4260e43efc6812fe9b3e
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-12-19 23:19:33 +0100

    modified esn

--- src/reservoir_computing_prediction.py
+++ src/reservoir_computing_prediction.py
@@ -13,13 +13,23 @@ from auto_esn.esn.reservoir.activation import tanh
 def test_esn(data, test_size, name):
     mse = []
     number_of_points = np.arange(40, 300, 20)
-    number_of_layers = [1, 2, 3]
+    number_of_layers = [1, 2, 4]
 
     for layers in  number_of_layers:
         mse = []
         for points_number in number_of_points:
-            seidr = dl.loader_explicit(data, test_size=int(data.size * test_size))
-            X, X_test, y, y_test = seidr()
+            sets = tuple([] for _ in range(4))
+            for column in data:
+                col = data[[column]].copy()
+                col.rename({column: "y"})
+                X_1, X_test_1, y_1, y_test_1 = dl.loader_explicit(col, test_size=int(data.size * test_size))
+                sets[0].append(X_1)
+                sets[1].append(X_test_1)
+                sets[2].append(y_1)
+                sets[3].append(y_test_1)
+            
+            [X, X_test, y, y_test] = [torch.cat((set), dim=1) for set in sets ]
+               
             step = int(data.size * (1 - test_size) / points_number)
             indices  = torch.arange(0, int(data.size * (1 - test_size)) - 1, step)
             X = X[indices]
@@ -28,14 +38,23 @@ def test_esn(data, test_size, name):
             esn.fit(X, y)
             output = esn(X_test)
             plt.clf()
-            plt.plot(range(int(data.size * test_size)), output.view(-1).detach().numpy(), 'r',label='predicted')
-            plt.plot(range(int(data.size * test_size)), y_test.view(-1).detach().numpy(), 'b',label='original')
+            plt.plot(range(int(data.size * test_size)), output[:, 0].view(-1).detach().numpy(), 'r',label='Susceptible Predicted')
+            plt.plot(range(int(data.size * test_size)), y_test[:, 0].view(-1).detach().numpy(), 'b',label='Susceptible Original')
+            plt.plot(range(int(data.size * test_size)), output[:, 1].view(-1).detach().numpy(), 'r',label='Exposed Predicted')
+            plt.plot(range(int(data.size * test_size)), y_test[:, 1].view(-1).detach().numpy(), 'b',label='Exposed Original')
+            plt.plot(range(int(data.size * test_size)), output[:, 2].view(-1).detach().numpy(), 'r',label='Infected Predicted')
+            plt.plot(range(int(data.size * test_size)), y_test[:, 2].view(-1).detach().numpy(), 'b',label='Infected Original')
+            plt.plot(range(int(data.size * test_size)), output[:, 3].view(-1).detach().numpy(), 'r',label='Fatalities Predicted')
+            plt.plot(range(int(data.size * test_size)), y_test[:, 3].view(-1).detach().numpy(), 'b',label='Fatalities Original')
+            plt.plot(range(int(data.size * test_size)), output[:, 4].view(-1).detach().numpy(), 'r',label='Recovered Predicted')
+            plt.plot(range(int(data.size * test_size)), y_test[:, 4].view(-1).detach().numpy(), 'b',label='Recovered Original')
             plt.legend()
             plt.savefig(f'../output/reservoir/{name}_layers_{layers}_train_{points_number}')
             current_mse = mean_squared_error(y_test, output)
             mse.append(current_mse)
+        x_labels = [num/data.size for num in number_of_points]
         plt.clf()
-        plt.plot(number_of_points, mse,'b',label='mse')
+        plt.plot(x_labels, mse,'b',label='mse')
         plt.xlabel("Train size")
         plt.ylabel("MSE")
         plt.legend()
@@ -80,4 +99,4 @@ if __name__ == "__main__":
     baseline = pd.read_csv('../output/baseline.csv')
     susceptible = pd.DataFrame(data=baseline['Susceptible'].tolist(), columns=['y'])
     test_esn(susceptible, 0.2, "esn")
-    test_grouped_esn(susceptible, 0.2, "grouped_esn")
+    # test_grouped_esn(susceptible, 0.2, "grouped_esn")
